61d4aa5fd742fe380e6289ac3f143870cf8d2eba
Mark-DSouza <markdsouza434@gmail.com>
2025-02-27 23:52:05 +0530

    Parsing JSON Requests

--- internal/data/runtime.go
+++ internal/data/runtime.go
@@ -1,8 +1,10 @@
 package data
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
+	"strings"
 )
 
 type Runtime int32
@@ -11,4 +13,27 @@ func (r Runtime) MarshalJSON() ([]byte, error) {
 	jsonValue := fmt.Sprintf("%d mins", r)
 	quotedJsonValue := strconv.Quote(jsonValue)
 	return []byte(quotedJsonValue), nil
+}
+
+var ErrInvalidRuntimeFormat = errors.New("invalid runtime format")
+
+func (r *Runtime) UnmarshalJSON(json []byte) error {
+	unquotedJsonValue, err := strconv.Unquote(string(json))
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	parts := strings.Split(unquotedJsonValue, " ")
+	if len(parts) != 2 && parts[1] != "mins" {
+		return ErrInvalidRuntimeFormat
+	}
+
+	i, err := strconv.ParseInt(parts[0], 10, 32)
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	*r = Runtime(i)
+
+	return nil
 }
